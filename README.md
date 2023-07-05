1ab77618347c4d0f694774635c8deaea7658e91b
superkikim <superkikim@sissaoui.com>
2023-07-05 12:10:48 +0200

    Adding documentation

---
+++ README.md
@@ -0,0 +1,53 @@
+# MaxSmart (Revogi) for Home Assistant
+
+This is a custom component for Home Assistant that integrates Max Hauri's MaxSmart Power Devices, including Smart Plug and Power Station. It communicates with the devices over your local network, providing controls for each port on the device.
+
+## Features
+
+* Discovery of MaxSmart devices on the network.
+* Control of the master switch as well as individual ports on each device.
+* Pulls device status updates periodically.
+
+## Installation
+
+This component is not included in the default Home Assistant installation. Therefore, it needs to be installed manually:
+
+1. Navigate to the `config` directory of your Home Assistant installation.
+2. If you don't have a `custom_components` directory inside the `config` directory, create it.
+3. Inside the `custom_components` directory, create another directory named `maxsmart`.
+4. Download the four files from this repository (`__init__.py`, `manifest.json`, `config_flow.py`, `switch.py`) and place them inside the `maxsmart` directory.
+
+After you have installed the component, you need to restart Home Assistant.
+
+## Configuration
+
+After installing the component, you need to add it to your configuration:
+
+1. Go to **Configuration** > **Integrations**.
+2. Click on the **Add Integration** button.
+3. Search for `MaxSmart (Revogi)` and select it.
+4. If the devices are not discovered automatically, you will be asked to input the IP address of the device.
+5. Once the devices are discovered, they will be added to your Home Assistant setup and can be controlled via the UI.
+
+## Entities
+
+Each discovered device will create a set of entities. There is one master switch entity and a separate switch entity for each individual port. The entity IDs will be generated based on the device's serial number and port number in the format `switch.maxsmart_<serialnumber>_<portnumber>`.
+
+## Notes
+
+* This integration is developed and tested with Home Assistant version 2023.6.2, but it should be compatible with other versions as well.
+* The devices are discovered via local network polling, which means the devices need to be on the same network as your Home Assistant instance.
+* The device state in Home Assistant is updated periodically based on polling.
+* The component depends on the `maxsmart` and `requests` Python libraries, which will be automatically installed.
+
+## Reporting Issues
+
+If you encounter issues with this custom component, please report them at the [issue tracker](https://github.com/superkikim/maxsmart/issues) on GitHub.
+
+## Further Development
+
+This component is under active development. Contributions, ideas, and suggestions are welcomed! Please reach out to the [codeowners](https://github.com/superkikim) for collaboration.
+
+Remember to refer to the latest Home Assistant developer documentation for up-to-date information when contributing to this project.
+
+**Disclaimer: This is a third-party integration and not officially supported by Home Assistant or Max Hauri. Use it at your own risk.**
---
+++ custom_components/maxsmart/config_flow.py
@@ -0,0 +1,102 @@
+import logging
+from homeassistant import config_entries, exceptions
+from homeassistant.const import CONF_IP_ADDRESS
+import voluptuous as vol
+from .const import DOMAIN
+from maxsmart import MaxSmartDiscovery
+
+_LOGGER = logging.getLogger(__name__)
+
+
+class MaxSmartConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
+    """MaxSmart Config Flow"""
+
+    VERSION = 1
+
+
+    async def async_step_user(self, user_input=None):
+        """Handle a flow initialized by the user."""
+        errors = {}
+
+        if user_input is None:
+            _LOGGER.info("Starting user-initiated device discovery without IP.")
+            devices = await self.hass.async_add_executor_job(
+                MaxSmartDiscovery.discover_maxsmart
+            )
+            _LOGGER.info(f"Discovered devices without IP: {devices}")
+        else:
+            _LOGGER.info("Starting user-initiated device discovery with IP.")
+            devices = await self.hass.async_add_executor_job(
+                MaxSmartDiscovery.discover_maxsmart, user_input[CONF_IP_ADDRESS]
+            )
+            _LOGGER.info(f"Discovered devices with IP: {devices}")
+
+        if devices:
+            _LOGGER.info("Devices have been found. Attempting to create entries")
+            for device in devices:
+                await self.hass.config_entries.flow.async_init(
+                    DOMAIN,
+                    context={"source": config_entries.SOURCE_IMPORT},
+                    data=device
+                )
+            return self.async_abort(reason="devices_found")
+        else:
+            return self.async_show_form(
+                step_id="user",
+                data_schema=vol.Schema({vol.Required(CONF_IP_ADDRESS): str}),
+                errors={"base": "no_devices_found"},
+            )
+
+    _LOGGER.info("Finished step user")
+
+    async def async_step_import(self, device):
+        """Create entry for a device"""
+        try:
+            pname = device.get("pname")
+            # sw_version = device.get("ver")
+            port_data = {}
+
+            if pname is None:
+                port_data = {
+                    "master": {"port_id": 0, "port_name": "0. Master"},
+                    "individual_ports": [
+                        {"port_id": 1, "port_name": "1. Port"}
+                    ],
+                }
+            else:
+                port_data = {
+                    "master": {"port_id": 0, "port_name": "0. Master"},
+                    "individual_ports": [
+                        {"port_id": i + 1, "port_name": f"{i + 1}. {port_name}"}
+                        for i, port_name in enumerate(pname)
+                    ],
+                }
+
+            device_data = {
+                "device_unique_id": device["sn"],
+                "device_ip": device["ip"],
+                "device_name": device['name'],
+                "sw_version": device["ver"],
+                "ports": port_data,
+            }
+
+            await self.async_set_unique_id(device["sn"])
+
+            current_entries = self._async_current_entries()
+            existing_entry = next((entry for entry in current_entries if entry.unique_id == device["sn"]), None)
+
+            if existing_entry:
+                _LOGGER.info("Device %s with name %s is already configured", device["sn"], device["name"])
+                if existing_entry.data != device_data:
+                    _LOGGER.info("Updating config entry for device %s", device["sn"])
+                    self.hass.config_entries.async_update_entry(existing_entry, data=device_data)
+                return self.async_abort(reason="device_already_configured")
+
+            return self.async_create_entry(
+                title=f"maxsmart_{device['sn']}",
+                data=device_data,
+            )
+
+        except Exception as err:
+            _LOGGER.error("Failed to create device entry: %s", err)
+
